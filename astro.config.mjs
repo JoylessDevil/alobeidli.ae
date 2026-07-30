39257dd667d140a9930b448494dc6cdbb6c95e61
JoylessDevil <666@joylessdevil.com>
2026-07-30 15:47:32 +0400

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-const repository = process.env.GITHUB_REPOSITORY ?? "";
-const [owner = "", name = ""] = repository.split("/");
-const userSite = name === `${owner}.github.io`;
-export default defineConfig({
-  site: owner ? `https://${owner}.github.io` : "http://localhost:4321",
-  base: owner && !userSite ? `/${name}` : "/",
-});
